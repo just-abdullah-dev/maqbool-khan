a596a4c8a8d4d078b5e68fd321ac9ec45b2115aa
Abdullah <abdulhaseeb9274@gmail.com>
2024-03-28 23:15:57 +0500

    worked on admin dashboard

--- src/components/Admin/Dashboard/Dashboard.jsx
+++ src/components/Admin/Dashboard/Dashboard.jsx
@@ -1,31 +1,120 @@
-import Link from 'next/link';
-import React from 'react'
+"use client";
+import {
+  BriefcaseIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  FileBadge2,
+  GraduationCap,
+  Images,
+  LayoutDashboard,
+  NotebookText,
+  SearchCheck,
+  Sparkles,
+  SquareGantt,
+  UserRound,
+  UserRoundCog,
+} from "lucide-react";
+import Link from "next/link";
+import React, { useState } from "react";
 
-export default function Dashboard({children}) {
-    const options = [
-        {name: "Profile", path: "dashboard/profile"},
-        {name: "Profile", path: "dashboard/profile"},
-        {name: "Profile", path: "dashboard/profile"},
-        {name: "Profile", path: "dashboard/profile"},
-        {name: "Profile", path: "dashboard/profile"},
-        {name: "Profile", path: "dashboard/profile"}        
-    ];
+export default function Dashboard({ children }) {
+  const [openSidebar, setOpenSidebar] = useState(true);
+  const options = [
+    {
+        name: "Dashboard",
+        path: "dashboard",
+        icon: <LayoutDashboard size={30} />,
+      },
+    {
+      name: "Profile",
+      path: "dashboard/profile",
+      icon: <UserRound size={30} />,
+    },
+    {
+      name: "Experience",
+      path: "dashboard/experience",
+      icon: <BriefcaseIcon size={30} />,
+    },
+    {
+      name: "Education",
+      path: "dashboard/education",
+      icon: <GraduationCap size={30} />,
+    },
+    {
+      name: "Skills",
+      path: "dashboard/skills",
+      icon: <UserRoundCog size={30} />,
+    },
+    {
+      name: "Publications",
+      path: "dashboard/publications",
+      icon: <NotebookText size={30} />,
+    },
+    {
+      name: "Certifications",
+      path: "dashboard/certifications",
+      icon: <FileBadge2 size={30} />,
+    },
+    {
+      name: "Specializations",
+      path: "dashboard/specializations",
+      icon: <Sparkles size={30} />,
+    },
+    {
+      name: "Projects",
+      path: "dashboard/projects",
+      icon: <SquareGantt size={30} />,
+    },
+    {
+      name: "Research",
+      path: "dashboard/research",
+      icon: <SearchCheck size={30} />,
+    },
+    { name: "Gallery", path: "dashboard/gallery", icon: <Images size={30} /> },
+  ];
 
   return (
-    <div className=' flex items-start'>
-        <div className=' w-[20%]'>
-        {options.map((opt, index)=>{
-            return <Link className='block'
-            href={opt?.path}
-            key={index}
+    <div className=" flex items-start">
+      <div
+        className={`${
+          openSidebar ? "w-[12%]" : "w-[4%] pl-"
+        } transition-all duration-1000 border-r h-screen border-black dark:border-white flex flex-col gap-4 overflow-hidden p-`}
+      >
+        <button
+          onClick={() => {
+            setOpenSidebar(!openSidebar);
+          }}
+          title={openSidebar ? "Close Sidebar" : "Open Sidebar"}
+        >
+          {openSidebar ?
+          
+        <div className=" flex items-center gap-4 bg-gray-800 text-gray-200 dark:bg-gray-200 dark:text-gray-800 py-2"><ChevronLeftIcon className="pl-2" size={38}/><p className="">Close Sidebar</p></div>:
+        <ChevronRightIcon className="pl-2" size={38}/>
+
+        }
+        </button>
+        {options.map((opt, index) => {
+          return (
+            <Link
+              className=" flex items-center gap-4 px-2"
+              href={opt?.path}
+              key={index}
+              title={opt?.name}
             >
-            {opt?.name}
+              {opt?.icon}
+              {openSidebar && opt?.name}
             </Link>
+          );
         })}
-        </div>
-        <div className=' w-[80%]'>
-            {children}
-        </div>
+      </div>
+      <div
+        className={`${
+          openSidebar ? "w-[88%]" : "w-[96%]"
+        } transition-all duration-1000 h-fit min-h-screen`}
+      >
+        {children}
+        
+      </div>
     </div>
-  )
+  );
 }
