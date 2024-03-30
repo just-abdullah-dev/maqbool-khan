d6a08011e264e7518169c898f6878e14f07873ab
Abdullah <abdulhaseeb9274@gmail.com>
2024-03-30 14:20:55 +0500

    eslint disabled

--- src/app/admin/pin/dashboard/page.js
+++ src/app/admin/pin/dashboard/page.js
@@ -1,19 +1,20 @@
 "use client";
 import Dashboard from '@/components/Admin/Dashboard/Dashboard'
 import React from 'react'
-import { useSelector } from 'react-redux'
+// import { useSelector } from 'react-redux';
 
 export default function page() {
-  let {userInfo} = useSelector((state)=> state.user);
-  userInfo = userInfo?.data;
-  console.log(userInfo);
+  // let {userInfo} = useSelector((state)=> state.user);
+  // userInfo = userInfo?.data;
   return (
     <div>
         <Dashboard>
             <div>
               <div>
                 <h1>Welcome Back!</h1>
-                <h1>{userInfo?.name?.title}. {userInfo?.name?.first}</h1>
+                {/* <h1>{userInfo?.name?.title}. {userInfo?.name?.first}</h1>
+                 */}
+                 Name
               </div>
             </div>
         </Dashboard>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,20 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        primary:"gray-900",
+        
+        primary_2:"gray-100",
+        secondary:'gray-200',
+        dark:{
+          hard:"#bababa",
+          grey:'#808080',
+          // soft:"#0D2436",
+        },
+        light:{
+          soft:'#d5d5d5',
+        }
+      },
     },
   },
   plugins: [],
