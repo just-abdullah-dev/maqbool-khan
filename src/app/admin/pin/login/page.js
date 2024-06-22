3a73d01570bf3ab5422fa6a97da3238fde0f95fe
Abdullah <abdulhaseeb9274@gmail.com>
2024-06-22 23:33:58 +0500

    now not fetching any data

--- src/app/admin/pin/login/page.js
+++
@@ -1,9 +0,0 @@
-'use client';
-import Login from '@/components/Admin/Login/Login';
-import React from 'react'
-
-export default function page() {
-  return (
-    <Login />
-  )
-}
--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,29 @@
 import Layout from "@/components/Layout/Layout";
-import Home from "@/components/Pages/Home/Home";
-import Socials from "@/components/Utils/SocialMediaIcons";
-import { getAll } from "@/services/utils";
+// import Home from "@/components/Pages/Home/Home";
+// import Socials from "@/components/Utils/SocialMediaIcons";
+// import { getAll } from "@/services/utils";
 
 export default async function Page() {
-  const aboutData = await getAll("maqboolkhan", "personal");
-  const eduData = await getAll("maqboolkhan", "education");
-  const publiData = await getAll("maqboolkhan", "publications");
-  const projectData = await getAll("maqboolkhan", "projects");
-  const expeData = await getAll("maqboolkhan", "experience");
-  const skillsData = await getAll("maqboolkhan", "skills");
-  const countriesData = await getAll("maqboolkhan", "gallery");
+  // const aboutData = await getAll("maqboolkhan", "personal");
+  // const eduData = await getAll("maqboolkhan", "education");
+  // const publiData = await getAll("maqboolkhan", "publications");
+  // const projectData = await getAll("maqboolkhan", "projects");
+  // const expeData = await getAll("maqboolkhan", "experience");
+  // const skillsData = await getAll("maqboolkhan", "skills");
+  // const countriesData = await getAll("maqboolkhan", "gallery");
 
   return (
     <Layout>
       <div className=" relative font-san">
-        <Socials
+        {/* <Socials
           css={
             " flex flex-col fixed top-36 z-50 right-0 text-whit bg-white dark:bg-gray-900 bg-opacity-50 dark:bg-opacity-50"
           }
           data={aboutData?.data?.socials}
           altNameCss={"right-full group-hover:right-[calc(100%+0.7rem)]"}
-        />
-        <Home
+        />  */}
+        home page
+        {/* <Home
           aboutData={aboutData}
           eduData={eduData}
           publiData={publiData}
@@ -30,7 +31,7 @@ export default async function Page() {
           expeData={expeData}
           skillsData={skillsData}
           countriesData={countriesData}
-        />
+        /> */}
       </div>
     </Layout>
   );
