44f2d0c5bf5a5c86dffe1eca535edbc64efe5ee3
Abdullah <abdulhaseeb9274@gmail.com>
2024-06-22 16:06:33 +0500

    commented some code :(

--- src/app/publications/page.js
+++ src/app/publications/page.js
@@ -1,11 +1,11 @@
 import Layout from '@/components/Layout/Layout'
-import Publications from '@/components/Pages/Publications/Publications'
+// import Publications from '@/components/Pages/Publications/Publications'
 // import Researchs from '@/components/Pages/Research/Researchs';
-import { getAll } from '@/services/utils'
+// import { getAll } from '@/services/utils'
 import React from 'react'
 
 export default async function page() {
-    const publiData = await getAll("maqboolkhan", "publications");
+    // const publiData = await getAll("maqboolkhan", "publications");
     // const researchData = await getAll("maqboolkhan", "research");
 // console.log(researchData);
   return (
@@ -14,7 +14,7 @@ export default async function page() {
         <button>Publications</button>
         <button>Research</button>
       </div>
-        <Publications data={publiData} />
+        {/* <Publications data={publiData} /> */}
         {/* <Researchs data={publiData} /> */}
     </Layout>
   )
