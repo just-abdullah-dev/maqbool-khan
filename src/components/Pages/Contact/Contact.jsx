ecc41af9924644231f759c56dcb22b00046d8c8c
Abdullah <abdulhaseeb9274@gmail.com>
2024-06-22 23:28:20 +0500

    update get all function

--- src/components/Pages/Contact/Contact.jsx
+++ src/components/Pages/Contact/Contact.jsx
@@ -32,7 +32,7 @@ export default function Contact() {
       toast.error("Please type a message");
       return;
     }
-    
+     
     setIsLoading(true);
     const options = {
       method: "POST",
--- src/services/utils.js
+++ src/services/utils.js
@@ -7,13 +7,44 @@ export async function revalidateTagFunc(tag){
   revalidateTag(tag)
 }
 
-export async function getAll(professorId, tableName){
+// export async function getAll(professorId, tableName){
+//   const requestOptions = {
+//       method: 'GET',
+//       redirect: "follow",
+//       next: { tags: [tableName] }
+//     };
+// const baseUrl = process.env.ENV === "production" ? "https://maqbool-khan.vercel.app" : process.env.ENV === "development" ? "http://localhost:3000" : "";
+//   const data = await fetch(`${baseUrl}/api/v1/${tableName}/${professorId}`, requestOptions);
+//   return data.json();
+// }
+
+export async function getAll(professorId, tableName) {
   const requestOptions = {
-      method: 'GET',
-      redirect: "follow",
-      next: { tags: [tableName] }
-    };
-const baseUrl = process.env.ENV === "production" ? "https://maqbool-khan.vercel.app" : process.env.ENV === "development" ? "http://localhost:3000" : "";
-  const data = await fetch(`${baseUrl}/api/v1/${tableName}/${professorId}`, requestOptions);
-  return data.json();
+    method: 'GET',
+    redirect: 'follow',
+    next: { tags: [tableName] }
+  };
+
+  const baseUrl = process.env.ENV === 'production' 
+    ? 'https://maqbool-khan.vercel.app' 
+    : process.env.ENV === 'development' 
+    ? 'http://localhost:3000' 
+    : '';
+
+  try {
+    const response = await fetch(`${baseUrl}/api/v1/${tableName}/${professorId}`, requestOptions);
+    
+    // Check if the response is ok (status is in the range 200-299)
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    // You can throw the error again or return a default value
+    throw error; // or return null; or return { error: error.message };
+  }
 }
+
